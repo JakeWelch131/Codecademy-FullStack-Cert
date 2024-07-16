6bfd6f898de4bafc3654fdf80d13f6cd316c35e7
JakeWelch131 <150453377+JakeWelch131@users.noreply.github.com>
2024-07-15 21:12:43 -0400

    Create app.js

---
+++ Building Interactive Websites/Dog Years/app.js
@@ -0,0 +1,20 @@
+//initializes myAge to 20
+const myAge = 20;
+
+//initializes earlyYears to 2
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+myAgeInDogYears = earlyYears + laterYears;
+
+myName = 'Jake'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
